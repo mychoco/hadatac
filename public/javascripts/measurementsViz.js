4a03a5eaf1e151ef0399b7fd4fde08512f6e07e1
paulopinheiro1234 <pp3223@gmail.com>
2015-08-18 21:40:46 -0400

    initializing measurment visualization branch

---
+++ public/javascripts/measurementsViz.js
@@ -0,0 +1,14 @@
+var query_res = document.getElementById('query');
+var results = query_res.dataset.results;
+
+var width = 1280,
+    height = 760;
+
+var color = d3.scale.category20();
+
+
+
+
+graph=JSON.parse(results);
+
+
